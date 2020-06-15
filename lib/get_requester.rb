0d072341c047776432fdcd6a95907bcc6f3ee02e
Richard Cho <96richardohc@gmail.com>
2020-06-15 09:08:16 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,15 @@
 # Write your code here
+
+require 'net/http'
+require 'open-uri'
+require 'json'
+class GetRequester 
+ 
+  def initialize(url)
+    @url = url 
+  end 
+  
+  def get_response_body
+    
+  end
+end
