a8cde51e886f32b0366f5a902158d3602a4a071e
Richard Cho <96richardohc@gmail.com>
2020-06-15 09:13:27 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -37,7 +37,7 @@ end
   
 #   def program_school
 # # we use the JSON library to parse the API response into nicely formatted JSON
-  programs = JSON.parse(self.get_programs)
+  # programs = JSON.parse(self.get_programs)
 #   programs.collect do |program|
 #     program["agency"]  
 #   end
